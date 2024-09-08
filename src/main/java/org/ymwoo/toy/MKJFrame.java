599a5c55ba0239fec1666631deaabeb3cd5c10b9
우영민 <ymwoo@hanatour.com>
2024-09-08 22:02:40 +0900

    마이너작업
    메모리관리를 위한 로그제한

--- src/main/java/org/ymwoo/toy/MKJFrame.java
+++ src/main/java/org/ymwoo/toy/MKJFrame.java
@@ -16,6 +16,7 @@ public class MKJFrame extends JFrame implements ActionListener, KeyListener {
     private static Integer START_TIME = 0;
     private static Integer END_TIME = 2359;
     private static int KEY_CODE = KeyEvent.VK_PRINTSCREEN;
+    private static int LIST_MODEL_DELTE_INDEX = 0;
     private JTextField startTime;
     private JTextField endTime;
     private JTextField bounceTime;
@@ -30,9 +31,16 @@ public class MKJFrame extends JFrame implements ActionListener, KeyListener {
     public MKJFrame() {
         setTitle("Toy of YMW v1.0");
 
+        JLabel notice = new JLabel("<html>오마키에 오신걸 환영합니다. <br> 메모리관리를위해 로그는 20라인까지만 기록됩니다.</html>");
+        notice.setOpaque(true);
+        notice.setBackground(Color.DARK_GRAY);
+        notice.setForeground(Color.WHITE);
+        JPanel north0 = new JPanel();
+        north0.add(notice);
+
         //문자열을 입력할수 있는 UI
-        JLabel label1 = new JLabel("반복간격(초) (범위 1 ~ 60)");
-        bounceTime = new JTextField("10", 2);
+        JLabel label1 = new JLabel("반복간격(초) (범위 5 ~ 60)");
+        bounceTime = new JTextField("5", 2);
         JLabel label2 = new JLabel("작동시간 (범위 0000 ~ 2359)");
         startTime = new JTextField("0000", 4);
         endTime = new JTextField("2359", 4);
@@ -42,6 +50,12 @@ public class MKJFrame extends JFrame implements ActionListener, KeyListener {
         north1.add(label2);
         north1.add(startTime);
         north1.add(endTime);
+
+        // Layout 중요한 부분
+        JPanel pg0 = new JPanel(new BorderLayout());
+        // 새로 추가한 컨테이너에 기존의 컨테이너를 올려주자.
+        pg0.add(north0, BorderLayout.NORTH);
+        pg0.add(north1, BorderLayout.CENTER);
 
         //문자열을 입력할수 있는 UI
         JLabel label3 = new JLabel("입력 키 지정 (커서위치 후 설정 키를 눌러주세요)");
@@ -66,7 +80,7 @@ public class MKJFrame extends JFrame implements ActionListener, KeyListener {
         JButton start = new JButton("시작");
         start.setActionCommand("start");
         start.addActionListener(this);
-        JButton stop = new JButton("종료");
+        JButton stop = new JButton("정지");
         stop.setActionCommand("stop");
         stop.addActionListener(this);
         JPanel south = new JPanel();
@@ -81,11 +95,12 @@ public class MKJFrame extends JFrame implements ActionListener, KeyListener {
         pg.add(south, BorderLayout.SOUTH);
 
         // 3. 컨테이너를 프레임에 올려주어야 한다.
-        add(north1, BorderLayout.NORTH);
+//        add(north1, BorderLayout.NORTH);
+        add(pg0, BorderLayout.NORTH);
         add(pg, BorderLayout.CENTER);
 
         // Default Config SET
-        setBounds(200, 200, 700, 500);
+        setBounds(300, 400, 700, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
 
@@ -118,25 +133,25 @@ public class MKJFrame extends JFrame implements ActionListener, KeyListener {
             // 시간정합성 체크
             if (!isNumericTime(start) || !isNumericTime(end)) {
                 JOptionPane.showMessageDialog(this, "0000 ~ 2359 시간범위로만 설정할 수 있습니다.");
-                model.addElement("[" + logTime + "] 오마키 동작시간 설정오류!");
+                setLog("[" + logTime + "] 오마키 동작시간 설정오류!");
                 return;
             }
             START_TIME = Integer.parseInt(startTime.getText());
             END_TIME = Integer.parseInt(endTime.getText());
 
             if (!isNumericBounce(bounceTime.getText())) {
-                JOptionPane.showMessageDialog(this, "1 ~ 60 간격범위로만 설정할 수 있습니다.");
-                model.addElement("[" + logTime + "] 오마키 동작간격 설정오류!");
+                JOptionPane.showMessageDialog(this, "5 ~ 60 간격범위로만 설정할 수 있습니다.");
+                setLog("[" + logTime + "] 오마키 동작간격 설정오류!");
                 return;
             }
             BOUNCE = Integer.parseInt(bounceTime.getText()) * 1000;
 
             ON_OFF = true;
-            model.addElement("[" + logTime + "] 오마키 시작! STATE[" + ON_OFF + "]");
-            model.addElement("[" + logTime + "] 동작시간범위 = " + START_TIME + " ~ " + END_TIME);
-            model.addElement("[" + logTime + "] 동작간격 = " + bounceTime.getText() + "초 마다 실행");
-            model.addElement("[" + logTime + "] 마우스가동범위 = (FIX) 1920x1080 랜덤");
-            model.addElement("[" + logTime + "] 키보드 입력키 = " + pressKey.getText());
+            setLog("[" + logTime + "] 오마키 시작! STATE[" + ON_OFF + "]");
+            setLog("[" + logTime + "] 동작시간범위 = " + START_TIME + " ~ " + END_TIME);
+            setLog("[" + logTime + "] 동작간격 = " + bounceTime.getText() + "초 마다 실행");
+            setLog("[" + logTime + "] 마우스가동범위 = (FIX) 1920x1080 랜덤");
+            setLog("[" + logTime + "] 키보드 입력키 = " + pressKey.getText());
         } else if (command.equals("stop")) {
             if (!ON_OFF) {
                 return;
@@ -145,8 +160,8 @@ public class MKJFrame extends JFrame implements ActionListener, KeyListener {
             ON_OFF = false;
             BOUNCE = 10000;
             // date
-            model.addElement("[" + logTime + "] 오마키 종료! STATE[" + ON_OFF + "]");
-            model.addElement("");
+            setLog("[" + logTime + "] 오마키 정지! STATE[" + ON_OFF + "]");
+            setLog("");
         }
         jScrollPane.getVerticalScrollBar().setValue(jScrollPane.getVerticalScrollBar().getMaximum());
     }
@@ -181,7 +196,7 @@ public class MKJFrame extends JFrame implements ActionListener, KeyListener {
                         log = "[" + logTime + "] 오마키가 동작중이나 가동시간범위가 아닙니다.";
                     }
 
-                    model.addElement(log);
+                    setLog(log);
                     jScrollPane.getVerticalScrollBar().setValue(jScrollPane.getVerticalScrollBar().getMaximum());
                 }
 
@@ -218,10 +233,20 @@ public class MKJFrame extends JFrame implements ActionListener, KeyListener {
 
         if (str.matches("[+-]?\\d*(\\.\\d+)?")) {
             int bounce = Integer.parseInt(str);
-            return 1 <= bounce && 60 >= bounce;
+            return 5 <= bounce && 60 >= bounce;
         } else {
             return false;
         }
+    }
+
+    /**
+     * 메모리 관리를위한 로그 관리
+     */
+    private void setLog(String log) {
+        if (model.size() == 20) {
+            model.remove(0);
+        }
+        model.addElement(log);
     }
 
     /**
@@ -245,7 +270,8 @@ public class MKJFrame extends JFrame implements ActionListener, KeyListener {
      */
     @Override
     public void keyReleased(KeyEvent e) {
-        model.addElement("[keyReleased] keyCode = [" + e.getKeyCode() + "] :: keyName = [" + KeyEvent.getKeyText(e.getKeyCode()) + "]");
+//        setLog("[keyReleased] keyCode = [" + e.getKeyCode() + "] :: keyName = [" + KeyEvent.getKeyText(e.getKeyCode()) + "]");
+        setLog("[keyReleased] keyCode = [" + e.getKeyCode() + "] :: keyName = [" + KeyEvent.getKeyText(e.getKeyCode()) + "]");
         pressKey.setText(KeyEvent.getKeyText(e.getKeyCode()));
         KEY_CODE = e.getKeyCode();
     }
